a233ca0df78dded7b7d9a4107dd6b44018b466d8
Michael Sault <michael_sault@hotmail.com>
2022-06-28 13:46:30 -0400

    examples of custom hooks in react
    useUser and the more general useResource are custom hooks that allow you to pull data from the server using UserInfo and ProductInfo

---
+++ Custom Hook Patterns/src/App.js
@@ -0,0 +1,13 @@
+import { UserInfo } from './UserInfo';
+import { ProductInfo} from './ProductInfo';
+
+function App() {
+	return (
+		<>
+			<UserInfo userId="345" />
+			<ProductInfo productId="2345" />
+		</>
+	);
+}
+
+export default App;
