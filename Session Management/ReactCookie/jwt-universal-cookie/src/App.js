0a4d378a4139d4794dddc2619fcb07f11273c5e3
MichaelSault <michael_sault@hotmail.com>
2023-03-17 22:20:51 -0400

    populates front end from cookie

--- Session Management/ReactCookie/jwt-universal-cookie/src/App.js
+++ Session Management/ReactCookie/jwt-universal-cookie/src/App.js
@@ -47,12 +47,17 @@ function App() {
         <div>
           <h1>Welcome {user.Username}</h1>
           <button onClick={logout}>Logout</button>
+
+          <p><b>Name:</b> {user.Name}</p>
+          <p><b>Email:</b> {user.Email}</p>
+          <p><b>City:</b> {user.City}</p>
+          <p><b>Favourite Dog Breed:</b> {user.DogBreed}</p>
         </div>
       ) : (
         <div>
-          <h1>Welcome Guest</h1>
+          <h1>Please Login to Continue...</h1>
           <button onClick={() => login(
-            "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoidXNlciIsIklzc3VlciI6IkRhdGFEb2ciLCJVc2VybmFtZSI6InVzZXIxIiwiZXhwIjoxNjgxMzI0NzEwLCJpYXQiOjE2Nzg2NDYzMTB9.9ino-T9qd93GL7l3ETB_ahd6T1uXhEbPY66ciiUnIQQ"
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEYXRhRG9nIiwiaWF0IjoxNjc5MTAyMjc0LCJleHAiOjE2Nzk3MDcwNzQsImF1ZCI6Ind3dy5kYXRhZG9nLmNvbSIsInN1YiI6ImRhdGFkb2dAaWNsb3VkLmNvbSIsIlVzZXJuYW1lIjoiSk1pdGNoZWxsIiwiRW1haWwiOiJKTWl0Y2hlbEBpY2xvdWQuY29tIiwiRG9nQnJlZWQiOiJBdXNzaWUiLCJBZ2UiOiIyNyIsIkNpdHkiOiJIYW1pbHRvbiIsIk5hbWUiOiJKYW1lcyBNaXRjaGVsbCJ9.0OWLcAwY0YH_LjNuRAo7YwopV9rpigBZaypzoewdOuM"
           )}>Login</button>
         </div>
       )} 
