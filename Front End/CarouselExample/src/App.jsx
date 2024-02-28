019af8c90b4a3980b0f47b5484b9db7702310f29
MichaelSault <michael_sault@hotmail.com>
2024-02-27 23:58:22 -0500

    added a carousel example

---
+++ Front End/CarouselExample/src/App.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import CarouselImage from "./components/CarouselImage";
+import Shadow from "./components/Shadow";
+import images from "./components/images";
+
+import "./slider.scss";
+
+const App = (props) => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [shadow, setShadow] = useState(true);
+
+  const getPrevIndex = (index) => {
+    let prev = index - 1;
+    if (prev < 0) prev = -1;
+    return prev;
+  };
+
+  const getNextIndex = (index) => {
+    let next = index + 1;
+    if (next >= images.length) next = -1;
+    return next;
+  };
+
+  const effectShadow = () => {
+    setShadow(false);
+    setTimeout(() => setShadow(true), 500);
+  };
+
+  const handlePrev = async () => {
+    effectShadow();
+    const prevIndex = getPrevIndex(selectedImg);
+    if (prevIndex < 0) {
+      console.log("asd");
+      for (let i = 0; i < images.length; i++) {
+        await Promise.resolve((resolve) => setTimeout(resolve, 1500));
+        setSelectedImg(i);
+      }
+    } else {
+      setSelectedImg(prevIndex);
+    }
+  };
+
+  const handleNext = () => {
+    effectShadow();
+    const nextIndex = getNextIndex(selectedImg);
+    if (nextIndex < 0) setSelectedImg(0);
+    else setSelectedImg(nextIndex);
+  };
+
+  return (
+    <div className="container">
+      <div className="carousel">
+        <div className="background" />
+        <button className="btn btn-border carousel__prev" onClick={handlePrev}>
+          PREV
+        </button>
+        <div className="carousel__container">
+          {images.map((img, index) => (
+            <CarouselImage
+              key={img.id}
+              image={img}
+              index={index}
+              currentIndex={selectedImg}
+            />
+          ))}
+
+          <Shadow show={shadow} />
+        </div>
+        <button className="btn btn-border carousel__next" onClick={handleNext}>
+          NEXT
+        </button>
+      </div>
+      <div className="carousel__container--index">
+        {images.map((_, index) => (
+          <span
+            key={`${index}dot`}
+            className={`${index === selectedImg && "selected"}`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Front End/CarouselExample/src/components/images.jsx
@@ -0,0 +1,28 @@
+const images = [
+    {
+      id: 1,
+      href: "https://suncityaz.org/wp-content/uploads/2016/11/DJI_0084.jpg"
+    },
+    {
+      id: 2,
+      href:
+        "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1800&h=1200&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F02%2Fvancouver-canada-02-CANADACITYWB20.jpg"
+    },
+    {
+      id: 3,
+      href:
+        "https://www.hotelscombined.com/news/wp-content/uploads/sites/314/2017/01/featured-image-GettyImages-1175037345-2.jpg"
+    },
+    {
+      id: 4,
+      href: "https://media4.manhattan-institute.org/sites/cj/files/hong-kong.jpg"
+    },
+    {
+      id: 5,
+      href:
+        "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1800&h=1200&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F04%2Fdubai-uae-08-AMECITIESWB20.jpg"
+    }
+  ];
+  
+  export default images;
+  
